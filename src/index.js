da09f9baefcb08c7d6b99e590f2ab481b72da701
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2022-08-15 23:10:22 +0200

    index js update

--- src/index.js
+++ src/index.js
@@ -9,11 +9,61 @@ const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-countrySearch.addEventListener('input', debounce(inputValue, DEBOUNCE_DELAY));
+const renderInfo = data => {
+  return data.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<img src="${flags.png}" alt="${name.official}" width="200" height="200">
+      <h1>${name.official}</h1>
+      <p>Capital: ${capital}</p>
+      <p>Population: ${population}</p>
+      <p>Languages: ${Object.values(languages)}</p>`,
+  );
+};
 
-function inputValue(event) {
-  let value = event.target.value.trim();
+const renderList = data => {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`,
+    )
+    .join('');
+};
 
+input.addEventListener('input', debounce(userValue,DEBOUNCE_DELAY));
 
+const userValue = e => {
+  const trimInput = e.target.value.trim();
+
+  if (trimInput != '') {
+    fetchCountries(trimInput)
+      .then(response => {
+        if (response.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+          countryInfo.innerHTML = '';
+          countryList.innerHTML = '';
+        }
+        if (response.length >= 2 && response.length <= 10) {
+          countryInfo.innerHTML = '';
+          renderList(countries);
+        }
+
+        if (response.length === 1) {
+          countryList.innerHTML = '';
+          renderInfo(countries);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+      });
+  } else {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+  }
+
+};
 
 
