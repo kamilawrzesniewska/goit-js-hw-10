783065e4b832da7f589efb0f87e697f9666d2168
kamilawrzesniewska <kamila.wrzesniewska07@gmail.com>
2022-08-19 22:19:46 +0200

    hw

--- src/index.js
+++ src/index.js
@@ -1,73 +1,61 @@
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
-import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-
-
-const input = document.querySelector("#search-box");
-const countryList = document.querySelector(".country-list");
-const countryInfo = document.querySelector(".country-info");
-
-function startData() {
-  countryList.innerHTML = '';
-  countryInfo.innerHTML = '';
-}
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries.js';
 
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector("#search-box");
+const countryListEl = document.querySelector(".country-list");
+const countryCardEl = document.querySelector(".country-info");
 
-input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-function renderInfo (country) {
-  const markupInfo = country
-    .map(({ name, capital, population, flags, languages }) => {
-      return `
-  <h1><img style="width: 50px; margin-right: 30px" src="${flags.svg}">${name.official}</h1>
-  <p><span>Capital: </span>${capital}</p>
-  <p><span>Population: </span>${population}</p>
-  <p><span>Languages: </span>${Object.values(languages)}</p>
-        `;
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+
+function onInput(event) {
+    const name = event.target.value.trim();
+    
+    if (!name) {
+        countryCardEl.innerHTML = '';
+        countryListEl.innerHTML = "";
+    }
+
+    fetchCountries(name).then(countries => {
+        if (countries.length === 1) {
+            countryListEl.innerHTML = "";
+            countryCardEl.innerHTML = createCountryCard(countries);
+            
+        }
+        else if (countries.length >= 2 && countries.length <= 10) {
+            countryCardEl.innerHTML = '';
+            countryListEl.innerHTML = createCountriesList(countries);
+        }
+        else {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        }
     })
+    .catch (() => Notiflix.Notify.failure('Oops, there is no country with that name'))   
+}
+    
+function createCountriesList(countries) {
+    return countries.map(({ flags, name }) =>
+    `<li>
+      <img src="${flags.svg}" alt="flag of ${name.official}" width = 30px; height = 15px;>
+        <span><b>${name.official}</b></span>
+    </li>`)
     .join('');
-
-    countryInfo.innerHTML = renderInfo;
 }
-
-const inputValue = evt => {
-  let { value } = evt.target;
-  value = value.trim().toLowerCase();
-  if (value) {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
-
-      fetchCountries(value).then(quantityСheck).catch((error) => {
-          console.log('error', error) 
-          Notify.failure('Oops, there is no country with that name');
-      });
-  }
-   if (value === '') {
-    countryInfo.innerHTML = '';
-      countryList.innerHTML = '';
-  }
-}
-
-function quantityСheck(data) {
-  if (data.length > 10) {
-      Notify.info('Too many matches found. Please enter a more specific name.');
-      return
-  }
-  if (data.length > 1) {
-      renderCountryList(data)
-  } else {
-      renderContryInfo(data)
-  }
- 
-}
-
-function renderCountryList(countries) {
-  const markup = countries
-      .map((country) => {
-          return `<li class='list-style'><p><img src="${country.flags.svg}" width = "20">  ${country.name.common}</p></li>`
-      }).join('');
-      countryList.innerHTML = markup;
+  
+function createCountryCard(countries) {
+  return countries.map(({ name, capital, population, flags, languages }) =>
+`
+  <ul>
+  <img src="${flags.svg}" alt="flag of ${name.official}" width = 100px; height = 50px;>
+  <span class="countryName"><b>${name.official}</b></span>
+  <li><span><b>Capital:</b></span> <span>${capital}</span></li>
+  <li><span><b>Population:</b></span> <span>${population}</span></li>
+  <li><span><b>Languages:</b></span><span>${Object.values(languages)}</span></li></ul>
+  `
+  ).join('');
 }
